3fbd9bcbca01ad052057803075fff95d2020bb46
Santhosh <sballa1@charlotte.edu>
2024-09-22 12:59:08 -0400

    54 min commit. There was a error last week, couldn't get past it. Finally got past it

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+# Create your views here.
+
+rooms = [{ 'id' : 1, 'name' : 'Lets learn python!'},
+         { 'id' : 2, 'name' : 'Design with me!'},
+          { 'id' : 3, 'name' : 'Frontend dev'}, 
+        ]
+
+def home(request):
+    context = {'rooms' : rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request):
+    return render(request, 'base/room.html')
+
